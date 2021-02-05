5a21566deba740eba5d664fc3233a08371252c53
kubs553 <33002823+kubs553@users.noreply.github.com>
2021-02-05 11:16:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,8 +10,10 @@ int main() {
   int licznik2 = 0;
   ifstream plik1("liczby1.txt");
   ifstream plik2("liczby2.txt");
-  for (int i = 0; i < rozmiar; i++) plik1 >> oct >> t1[i];
-  for (int i = 0; i < rozmiar; i++) plik2 >> t2[i];
+  for (int i = 0; i < rozmiar; i++) 
+    plik1 >> oct >> t1[i];
+  for (int i = 0; i < rozmiar; i++) 
+    plik2 >> t2[i];
   for (int i = 0; i < rozmiar; i++) {
     if (t1[i] == t2[i]) licznik1++;
     if (t1[i] > t2[i]) licznik2++;
